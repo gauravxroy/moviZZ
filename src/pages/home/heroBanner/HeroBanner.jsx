6a51ecf317245df8f2c9859c28b317355ae703ee
gauravxroy <96660449+gauravxroy@users.noreply.github.com>
2023-09-30 21:30:04 +0530

    Hero Banner added

---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,60 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.scss";
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
+import Img from "../../../components/lazyLoadImage/Img";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { data, loading } = useFetch("/movie/upcoming");
+  const { url } = useSelector((state) => state.home);
+  useEffect(() => {
+    const bg =
+      url.backdrop +
+      data?.results?.[Math.floor(Math.random() * 19)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
+
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+  const searchQueryHandlerOnclick = () => {
+    if (query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div className="heroBanner">
+      <div className="backdrop-img">
+        <Img src={background} />
+      </div>
+      <div className="opacityLayer"></div>
+      <ContentWrapper>
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Explore and watch millions of movies and shows
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search Movies,Shows..."
+              onKeyUp={searchQueryHandler}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button onClick={searchQueryHandlerOnclick}>Search</button>
+          </div>
+        </div>
+      </ContentWrapper>
+    </div>
+  );
+};
+
+export default HeroBanner;
