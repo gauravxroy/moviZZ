3728f3929602b873e816e63d1743959eb996b8b8
gauravxroy <96660449+gauravxroy@users.noreply.github.com>
2023-09-30 22:36:33 +0530

    App files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,62 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+import "./App.css";
+import {} from "./Stores/Store";
+import { fetchDataFromTMDb } from "./utils/api";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration } from "./Stores/homeSlice";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Explore from "./pages/explore/Explore";
+import Details from "./pages/details/Details";
+import PageNotFound from "./pages/404/PageNotFound";
+import SearchResult from "./pages/searchResult/SearchResult";
+import About from "./pages/about/About";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { url } = useSelector((state) => state.home);
+  const dispatch = useDispatch();
+  // console.log(url);
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
+  const fetchApi = () => {
+    fetchDataFromTMDb("/configuration")
+      .then((res) => {
+        // console.log(res);
+        const url = {
+          backdrop: res.images.secure_base_url + "original",
+          poster: res.images.secure_base_url + "original",
+          profile: res.images.secure_base_url + "w185",
+        };
+        dispatch(getApiConfiguration(url));
+      })
+
+      .catch((error) => {
+        console.error("API Error:", error); // Log any errors
+      });
+  };
 
   return (
     <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:mediaType/:id" element={<Details />} />
+          <Route path="/search/:query" element={<SearchResult />} />
+          <Route path="/explore/:mediaType" element={<Explore />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
 
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          {count}
-        </button>
-
-      </div>
-
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
